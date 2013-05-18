659fc97ae2728de67bbc9b7a10a87ac10e060a8f
ithlony <hzithlony@gmail.com>
2013-05-19 07:31:45 +0800

    Update background.js
    icon varies as mode changes

--- background.js
+++ background.js
@@ -1,13 +1,22 @@
+// Called when the user clicks on the browser action.
 localStorage.setItem("MODE", "MANUAL");
 chrome.browserAction.onClicked.addListener(function(tab) {
 	var mode = localStorage.getItem("MODE");
 	//alert(mode);
   if (mode == null || mode == "MANUAL")
+  {
+  	chrome.browserAction.setIcon({path: "Y.jpg"});
+  	chrome.browserAction.setTitle({title: "Auto Search: ON"});
   	localStorage.setItem("MODE", "AUTO");
-  else 
+  }
+  else {
+  	chrome.browserAction.setIcon({path: "Y2.jpg"});
+  	chrome.browserAction.setTitle({title: "Auto Search: OFF"});
   	localStorage.setItem("MODE", "MANUAL");
+  }
   //alert("click!");
 });
+
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	
